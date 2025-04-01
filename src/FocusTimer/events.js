b467ab85e1a5930065108e99fc7bcdf5fcb6f6fa
Ana Claudia <anacnogueira@gmail.com>
2025-04-01 09:06:44 -0300

    Finalizando a escolha dos minutoes do temporazidor

--- src/FocusTimer/events.js
+++ src/FocusTimer/events.js
@@ -1,6 +1,8 @@
 import { controls } from "./elements.js";
 import * as actions from "./actions.js";
 import * as el from "./elements.js";
+import { updateDisplay } from "./timer.js";
+import state from "./state.js";
 
 export function registerControls() {
   controls.addEventListener("click", (event) => {
@@ -20,4 +22,13 @@ export function setMinutes() {
   });
 
   el.minutes.onkeypress = (event) => /\d/.test(event.key);
+  el.minutes.addEventListener("blur", (event) => {
+    let time = event.currentTarget.textContent;
+    time = time > 60 ? 60 : time;
+
+    state.minutes = time;
+    state.seconds = 0;
+  });
+
+  updateDisplay();
 }
