39a9f026531aee4a1a9133b886157b999f1263fa
Ana Claudia <anacnogueira@gmail.com>
2025-04-01 09:26:02 -0300

    Adicionando sosn na aplicação

--- src/FocusTimer/timer.js
+++ src/FocusTimer/timer.js
@@ -1,6 +1,7 @@
 import state from "./state.js";
 import * as el from "./elements.js";
 import { reset } from "./actions.js";
+import { kitchenTimer } from "./sounds.js";
 
 export function updateDisplay(minutes, seconds) {
   minutes = minutes ?? state.minutes;
@@ -26,6 +27,7 @@ export function countdown() {
 
   if (minutes < 0) {
     reset();
+    kitchenTimer.play();
     return;
   }
 
